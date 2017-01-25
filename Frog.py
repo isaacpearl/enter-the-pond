ab0a5ca7f5acf82ab708d96cc83efcf56e3357e7
isaaacp <isaacpearl@Isaacs-MacBook-Pro.local>
2017-01-25 12:30:31 -0500

    first commit
    backend python code, brython js library, and abstract unicode
    representation of flocking algorithm

---
+++ Frog.py
@@ -0,0 +1,40 @@
+import random
+
+class frog:
+    def __init__(self, frogName, startingCode, allCodes):
+        self.name = frogName
+        self.code = startingCode
+        self.codeDict = allCodes
+        self.call = self.codeToCall()
+
+    def getName(self):
+        return self.name
+
+    def getCode(self):
+        return self.code
+
+    def getCall(self):
+        return self.call
+
+    def refreshCall(self):
+        self.call = self.codeToCall()
+
+    def setCode(self, newCode):
+        self.code = newCode
+        self.refreshCall() #i think? maybe take this out
+
+    def codeToCall(self):
+        result = []
+        for char in self.code:
+            if char == 'x':
+                randomIndex = random.randint(0, len(self.codeDict['x'])-1)
+                result.append(self.codeDict['x'][randomIndex]+' ')
+            else:
+                result.append(self.codeDict[char]+' ')
+            random.shuffle(result)
+        return ''.join(result)
+
+    def getCallList(self):
+        toReturn = self.call.split(sep=' ')
+        toReturn.remove('') #hacky fix for weird empty string that works
+        return toReturn
